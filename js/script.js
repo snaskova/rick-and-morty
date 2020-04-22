afda4fd5738d8a3c6b92145686d2b2efa9465841
Stef Naskova <snaskova094@gmail.com>
2020-04-22 18:07:51 +0200

    Init

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//  CREATE CHARACTER CARD
+const createCard = e => {
+    let name = e.name;
+    let image = e.image;
+    let species = e.species;
+    let origin = e.origin.name;
+
+    var output = ` <div class="card">
+                    <img src="${image}" alt="Thumbnail image for ${name}">
+                    <div class="info">
+                        <h3 class="name"><span class="bold">${name}</span></h2>
+                        <p class="species">Species: <span class="bold">${species}</span></p>
+                        <p class="origin">Origin: <span class="bold">${origin}</span></p>
+                    </div>
+                </div>`;
+
+    return output;
+}
+
+$(function () {
+
+    // GET INFO FOR CHARACTERS 
+    const getCharacters = number => {
+        $.get(`https://rickandmortyapi.com/api/character/?page=${number}`, resp => {
+            let result = "";
+            let base = resp.results;
+            // console.log(resp);
+
+            base.forEach(e => {
+                result += createCard(e);
+                $('#content').html(result);
+            });
+        })
+    }
+
+    getCharacters(1);
+
+    // PAGINATION LOGIC
+    $('.page-button').click(function (e) {
+        let thisButton = $(this).data('id');
+        getCharacters(thisButton);
+
+        let $siblings = $(this).siblings();
+        console.log(e.target.siblings());
+
+        if ($(this).hasClass('active')) {
+            $siblings.removeClass('active')
+        }
+
+    })
+
+    $.get(`https://rickandmortyapi.com/api/location`, resp => {
+    })
+})
